e6edc98f71d4bcb389531b6a7875dabcdffc0ef8
Himanshu Sinha <ashusinha1904@gmail.com>
2024-12-26 01:43:54 +0530

    Interviews page UI complete

--- src/app/dashboard/interviews/page.tsx
+++ src/app/dashboard/interviews/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import InterviewCard from "@/components/dashboard/InterviewCard";
 
 const Interviews = () => {
-  return <div>Interviews</div>;
+  const dummyInterviews = [
+    {
+      jobTitle: "React Interview",
+      experience: 5,
+      status: "ongoing",
+    },
+    {
+      jobTitle: "React Native Interview",
+      experience: 2,
+      status: "complete",
+    },
+    {
+      jobTitle: "Java Interview",
+      experience: 8,
+      status: "not_started",
+    },
+  ];
+  return (
+    <section className="mt-6 flex flex-col gap-y-6">
+      <div className="flex gap-x-4">
+        <Input placeholder="Search by job title..." />
+        <Button>New Interview</Button>
+      </div>
+
+      <div className="flex gap-4 items-center flex-wrap">
+        {dummyInterviews.map((interview) => {
+          return (
+            <InterviewCard interview={interview} key={interview.jobTitle} />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Interviews;
---
+++ src/components/dashboard/InterviewCard.tsx
@@ -0,0 +1,42 @@
+import { Trash2 } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+const InterviewCard = ({ interview }: any) => {
+  return (
+    <div className="border border-gray-300 w-[98%] sm:w-[410px] rounded-lg py-3 px-6 flex flex-col gap-y-5">
+      <div className="flex flex-col gap-y-1">
+        <div className="flex justify-between items-center">
+          <p className="text-lg font-bold text-primary">{interview.jobTitle}</p>
+          <Button
+            variant={"ghost"}
+            className="text-black hover:text-destructive delay-100 transition-all"
+          >
+            <Trash2 size={20} />
+          </Button>
+        </div>
+        <p className="text-justify font-medium">
+          <span className="font-semibold">{interview.experience}</span> years of
+          experience
+        </p>
+        <p className="text-justify text-sm text-gray-700">
+          Created 10 days ago
+        </p>
+      </div>
+
+      {interview.status === "complete" ? (
+        <Button className="w-full" variant={"secondary"}>
+          Results
+        </Button>
+      ) : interview.status === "ongoing" ? (
+        <p className="text-sm text-emerald-600 font-semibold text-center">
+          Ongoing
+        </p>
+      ) : (
+        <Button className="w-full">Start interview</Button>
+      )}
+    </div>
+  );
+};
+
+export default InterviewCard;
