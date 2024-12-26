96b83ef42ac51bb270cb827c36dc11360c521452
Himanshu Sinha <ashusinha1904@gmail.com>
2024-12-26 22:00:55 +0530

    Nodemailer and create otp function added

---
+++ src/helpers/create-otp.ts
@@ -0,0 +1,33 @@
+import "server-only";
+
+import { generate } from "otp-generator";
+
+import prisma from "@/lib/db";
+import { sendEmail } from "@/lib/send-email";
+
+export const createOtp = async (email: string) => {
+  const otp = parseInt(
+    generate(6, {
+      digits: true,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+      upperCaseAlphabets: false,
+    })
+  );
+
+  await prisma.otp.deleteMany({
+    where: {
+      userEmail: email,
+    },
+  });
+
+  await prisma.otp.create({
+    data: {
+      userEmail: email,
+      otp,
+      expiresIn: new Date(Date.now() + 1000 * 60 * 5),
+    },
+  });
+
+  await sendEmail(email, otp);
+};
