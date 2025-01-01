43a61850efeb615c06e998b69ee9ed3c78f9fb7f
Himanshu Sinha <ashusinha1904@gmail.com>
2025-01-01 21:43:57 +0530

    forgot password page complete

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,12 +1,64 @@
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import toast from "react-hot-toast";
+
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+
+import {
+  emailValidator,
+  type emailValidatorType,
+} from "@/validators/email-validator";
 
 const ForgotPassword = () => {
+  const router = useRouter();
+
+  const {
+    register,
+    reset,
+    formState: { errors },
+    handleSubmit,
+    getValues,
+  } = useForm<emailValidatorType>({
+    defaultValues: {
+      email: "",
+    },
+    resolver: zodResolver(emailValidator),
+  });
+
+  const { mutate: handleForgotPassword, isPending } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: async (values: emailValidatorType) => {
+      const { data } = await axios.post(`/api/forgot-password`, {
+        ...values,
+      });
+
+      return data as { message: string };
+    },
+    onSuccess: async (data) => {
+      toast.success(data.message);
+      router.replace(`/forgot-password/verify?email=${getValues("email")}`);
+      reset();
+    },
+    onError: async (error) => {
+      if (error instanceof AxiosError && error.response?.data.error) {
+        toast.error(error.response.data.error);
+      } else {
+        toast.error("Some error occured. Please try again later!");
+      }
+    },
+  });
   return (
     <>
-      <form className="flex flex-col gap-y-7 w-[90%]">
+      <form
+        className="flex flex-col gap-y-7 w-[90%]"
+        onSubmit={handleSubmit((data) => handleForgotPassword(data))}
+      >
         <p className="text-primary text-xl font-semibold text-center">
           Forgot password
         </p>
@@ -20,10 +72,16 @@ const ForgotPassword = () => {
             type="email"
             id="email"
             required
+            {...register("email", { required: true })}
           />
+          {errors.email && (
+            <p className="text-rose-600 text-sm">{errors.email.message}</p>
+          )}
         </div>
 
-        <Button type="submit">Submit</Button>
+        <Button type="submit" disabled={isPending}>
+          {isPending ? "Please wait..." : "Submit"}
+        </Button>
       </form>
 
       <div className="flex flex-col gap-y-4 items-center">
--- src/validators/email-validator.ts
+++ src/validators/email-validator.ts
@@ -5,3 +5,5 @@ export const emailValidator = z.object({
     .string({ required_error: "Email is required" })
     .email({ message: "Please enter a valid email" }),
 });
+
+export type emailValidatorType = z.infer<typeof emailValidator>;
