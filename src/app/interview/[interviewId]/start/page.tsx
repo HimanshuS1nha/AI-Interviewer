f2d4165c3155e0022a5077985ba129522fde61db
Himanshu Sinha <ashusinha1904@gmail.com>
2025-01-03 22:46:19 +0530

    New page added

---
+++ src/app/interview/[interviewId]/start/page.tsx
@@ -0,0 +1,13 @@
+const InterviewStart = () => {
+  return (
+    <section className="flex flex-col gap-y-6">
+      <h1 className="text-2xl text-primary font-semibold text-center">
+        React.js Interview
+      </h1>
+
+      <div className="flex w-full h-full gap-x-3"></div>
+    </section>
+  );
+};
+
+export default InterviewStart;
