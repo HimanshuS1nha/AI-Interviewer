55c782fd6646a2c9bfd07dd8154bc296374b2159
Himanshu Sinha <ashusinha1904@gmail.com>
2025-01-04 22:10:20 +0530

    CreateInterviewDialog updated

--- src/components/dashboard/CreateInterviewDialog.tsx
+++ src/components/dashboard/CreateInterviewDialog.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import toast from "react-hot-toast";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -12,6 +18,11 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
+import {
+  createCompanyInterviewValidator,
+  type createCompanyInterviewValidatorType,
+} from "@/validators/create-company-interview-validator";
+
 const CreateInterviewDialog = ({
   isVisible,
   setIsVisible,
@@ -19,6 +30,24 @@ const CreateInterviewDialog = ({
   isVisible: boolean;
   setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm<createCompanyInterviewValidatorType>({
+    defaultValues: {
+      candidatesEmails: [],
+      experience: "",
+      jobDescription: "",
+      jobTitle: "",
+      techStack: "",
+      time: "",
+    },
+    resolver: zodResolver(createCompanyInterviewValidator),
+  });
+
   return (
     <Dialog open={isVisible} onOpenChange={() => setIsVisible((prev) => !prev)}>
       <DialogContent className="max-h-[90%] overflow-y-auto">
@@ -32,7 +61,15 @@ const CreateInterviewDialog = ({
             <Label className="ml-1" htmlFor="jobTitle">
               Job Title
             </Label>
-            <Input placeholder="Enter job title" id="jobTitle" required />
+            <Input
+              placeholder="Enter job title"
+              id="jobTitle"
+              required
+              {...register("jobTitle", { required: true })}
+            />
+            {errors.jobTitle && (
+              <p className="text-sm text-rose-600">{errors.jobTitle.message}</p>
+            )}
           </div>
           <div className="flex flex-col gap-y-3">
             <Label className="ml-1" htmlFor="jobDescription">
@@ -42,7 +79,13 @@ const CreateInterviewDialog = ({
               placeholder="Enter job description"
               id="jobDescription"
               required
+              {...register("jobDescription", { required: true })}
             />
+            {errors.jobDescription && (
+              <p className="text-sm text-rose-600">
+                {errors.jobDescription.message}
+              </p>
+            )}
           </div>
           <div className="flex flex-col gap-y-3">
             <Label className="ml-1" htmlFor="techStack">
@@ -51,7 +94,17 @@ const CreateInterviewDialog = ({
                 (separate by commas)
               </span>
             </Label>
-            <Input placeholder="Enter tech stack" id="techStack" required />
+            <Input
+              placeholder="Enter tech stack"
+              id="techStack"
+              required
+              {...register("techStack", { required: true })}
+            />
+            {errors.techStack && (
+              <p className="text-sm text-rose-600">
+                {errors.techStack.message}
+              </p>
+            )}
           </div>
           <div className="flex flex-col gap-y-3">
             <Label className="ml-1" htmlFor="experience">
@@ -63,7 +116,13 @@ const CreateInterviewDialog = ({
               type="number"
               id="experience"
               required
+              {...register("experience", { required: true })}
             />
+            {errors.experience && (
+              <p className="text-sm text-rose-600">
+                {errors.experience.message}
+              </p>
+            )}
           </div>
           <div className="flex flex-col gap-y-3">
             <Label className="ml-1" htmlFor="time">
@@ -75,7 +134,11 @@ const CreateInterviewDialog = ({
               type="number"
               id="time"
               required
+              {...register("time", { required: true })}
             />
+            {errors.time && (
+              <p className="text-sm text-rose-600">{errors.time.message}</p>
+            )}
           </div>
           <div className="flex flex-col gap-y-3">
             <Label className="ml-1" htmlFor="candidatesEmails">
@@ -88,7 +151,18 @@ const CreateInterviewDialog = ({
               placeholder="Enter candidates' emails"
               id="candidatesEmails"
               required
+              onChange={(e) =>
+                setValue(
+                  "candidatesEmails",
+                  e.target.value.split(",").map((item) => item.trim())
+                )
+              }
             />
+            {errors.candidatesEmails && (
+              <p className="text-sm text-rose-600">
+                {errors.candidatesEmails.message}
+              </p>
+            )}
           </div>
 
           <Button type="submit">Create</Button>
---
+++ src/validators/create-company-interview-validator.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const createCompanyInterviewValidator = z.object({
+  jobTitle: z
+    .string({ required_error: "Job title is required" })
+    .trim()
+    .min(1, { message: "Job title is required" }),
+  jobDescription: z
+    .string({ required_error: "Job description is required" })
+    .trim()
+    .min(1, { message: "Job description is required" }),
+  techStack: z
+    .string({ required_error: "Tech Stack is required" })
+    .trim()
+    .min(1, { message: "Tech Stack is required" }),
+  experience: z
+    .string({ required_error: "Experience is required" })
+    .trim()
+    .min(1, { message: "Experience is required" }),
+  time: z
+    .string({ required_error: "Total time is required" })
+    .trim()
+    .min(1, { message: "Total time is required" }),
+  candidatesEmails: z
+    .array(z.string())
+    .min(1, { message: "Add atleast one candidate email" }),
+});
+
+export type createCompanyInterviewValidatorType = z.infer<
+  typeof createCompanyInterviewValidator
+>;
