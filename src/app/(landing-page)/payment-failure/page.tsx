4c4563996028cb66a001a0311ce1c0738c4d0534
Himanshu Sinha <ashusinha1904@gmail.com>
2025-01-23 23:04:31 +0530

    Payment workflow complete

--- src/app/(landing-page)/payment-failure/page.tsx
+++ src/app/(landing-page)/payment-failure/page.tsx
@@ -3,23 +3,23 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
-const PaymentSuccess = () => {
+const PaymentFailure = () => {
   return (
     <section className="flex flex-col pt-10 pb-10 lg:pb-0 lg:h-[80vh] items-center">
       <div className="lg:w-[70%] h-full flex flex-col lg:flex-row justify-between items-center gap-y-10">
         <div className="flex flex-col gap-y-7 items-center lg:items-start">
           <div className="flex flex-col gap-y-4 items-center lg:items-start">
-            <p className="text-3xl md:text-5xl font-bold text-green-600 text-center">
-              Payment Successful!
+            <p className="text-3xl md:text-5xl font-bold text-rose-600 text-center">
+              Payment Failed!
             </p>
-            <p className="text-gray-700 text-justify w-[75%] text-sm">
-              Thank you for the payment. You can now create more interviews with
-              an even more number of candidates.
+            <p className="text-gray-700 text-justify w-[90%] text-sm">
+              If you think this is a mistake on our part, please reach out to
+              us.
             </p>
           </div>
           <div className="flex items-center gap-x-4">
-            <Button variant={"default"} asChild>
-              <Link href={"/dashboard"}>Go to Dashboard</Link>
+            <Button variant={"destructive"} asChild>
+              <Link href={"/contact-us"}>Contact Us</Link>
             </Button>
             <Button variant={"ghost"} asChild>
               <Link href={"/"}>Back to Home</Link>
@@ -28,7 +28,7 @@ const PaymentSuccess = () => {
         </div>
         <div className="flex justify-center">
           <Image
-            src={"/success-gif.gif"}
+            src={"/failure-gif.gif"}
             alt="Success GIF"
             width={400}
             height={200}
@@ -41,4 +41,4 @@ const PaymentSuccess = () => {
   );
 };
 
-export default PaymentSuccess;
+export default PaymentFailure;
