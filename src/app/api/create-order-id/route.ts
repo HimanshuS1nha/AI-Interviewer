9f24a9919edfc536b933aca316c1252fb27771f1
Himanshu Sinha <ashusinha1904@gmail.com>
2025-01-23 22:46:56 +0530

    create-order-id api route added

---
+++ src/app/api/create-order-id/route.ts
@@ -0,0 +1,61 @@
+import Razorpay from "razorpay";
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { jwtVerify } from "jose";
+
+import prisma from "@/lib/db";
+
+import { getUserByEmail } from "@/helpers/get-user-by-email";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const token = (await cookies()).get("token")?.value;
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { payload } = (await jwtVerify(
+      token,
+      new TextEncoder().encode(process.env.JWT_SECRET)
+    )) as { payload: { email: string } };
+
+    const user = await getUserByEmail(payload.email);
+    if (!user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { amount } = await req.json();
+    if (amount !== 500) {
+      return NextResponse.json({ error: "Amount is invalid" }, { status: 422 });
+    }
+
+    const razorpay = new Razorpay({
+      key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY!,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+
+    const order = await razorpay.orders.create({
+      amount,
+      currency: "INR",
+      receipt: "receipt-123",
+    });
+
+    await prisma.paymentDetails.create({
+      data: {
+        amount,
+        orderId: order.id,
+        userEmail: user.email,
+      },
+    });
+
+    return NextResponse.json(
+      { orderId: order.id, amount: order.amount },
+      { status: 201 }
+    );
+  } catch {
+    return NextResponse.json(
+      { error: "Some error occured. Please try again later!" },
+      { status: 500 }
+    );
+  }
+};
