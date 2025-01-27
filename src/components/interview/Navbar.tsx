5f54ac69750e229151636b1a91defd0249aef2b2
Himanshu Sinha <ashusinha1904@gmail.com>
2025-01-27 22:00:22 +0530

    Bug fix

--- src/components/interview/Navbar.tsx
+++ src/components/interview/Navbar.tsx
@@ -1,17 +1,51 @@
+"use client";
+
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter, useParams } from "next/navigation";
+
 import BrandLogo from "@/components/BrandLogo";
+import Loader from "@/components/Loader";
 
 import { useCandidateEmail } from "@/hooks/useCandidateEmail";
-import { useParams } from "next/navigation";
 
 const Navbar = () => {
-  const { interviewId } = useParams() as { interviewId: string };
+  const router = useRouter();
+  const params = useParams() as { interviewId: string };
 
-  const candidateEmail = useCandidateEmail((state) => state.email);
+  const { email, setEmail } = useCandidateEmail();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["is-logged-in"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `/api/interview/${params.interviewId}/is-candidate-logged-in`
+      );
+      return data as { email: string };
+    },
+    retry: 1,
+  });
+
+  useEffect(() => {
+    if (data?.email) {
+      setEmail(data.email);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (error) {
+      router.replace("/");
+    }
+  }, [error]);
   return (
     <nav className="flex justify-between items-center h-[10vh]">
       <BrandLogo />
-
-      <p className="font-semibold">{candidateEmail}</p>
+      {isLoading ? (
+        <Loader size="sm" />
+      ) : (
+        <p className="font-semibold">{email}</p>
+      )}
     </nav>
   );
 };
