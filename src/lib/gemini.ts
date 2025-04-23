a13502c86f074cd3144ecc59b12ce507a8b455d1
Himanshu Sinha <ashusinha1904@gmail.com>
2025-04-23 15:13:46 +0530

    Updated to gemini-2.0-flash

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -4,4 +4,4 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
 
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
